df13ea9332f86ccb5afcb5aded5da7a091fb91ea
YoukaiYoru <edward.oliva.l@uni.pe>
2025-06-17 12:07:45 -0500

    FIX: Adjust branding

--- src/pages/HomeLoading.tsx
+++ src/pages/HomeLoading.tsx
@@ -5,14 +5,14 @@ export default function Home() {
   return (
     <>
       {/* Sección hero responsive */}
-      <section className="flex flex-col items-center h-screen justify-center md:h-[calc(100vh-64px)] bg-black md:flex-row">
+      <section className="flex flex-col items-center justify-center h-[calc(100vh-64px)] bg-black md:flex-row">
         {/* Bloque 1 */}
-        <div className="flex-1 flex items-center justify-center m-5 md:m-0">
+        <div className="flex-1 flex justify-center">
           <HeroBlocks />
         </div>
 
         {/* Bloque 2 */}
-        <div className="flex-1 flex items-center justify-center  m-5 md:m-0">
+        <div className="flex-1 flex items-center justify-center m-5 md:m-0 w-full md:w-auto">
           <LoadingAlpaca />
         </div>
       </section>
